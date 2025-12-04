e0a018f7a4fc943a4e7dd84a4787619345623e1a
petesammon <167710750+petesammon@users.noreply.github.com>
2025-12-04 08:59:08 +0000

    correct changes

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-let numButtonClicks = 0;
-function buttonClicked() {
-    numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Button Clicked times: " + numButtonClicks;
-}
+function toggle(element) {
+            const content = element.nextElementSibling;
+            
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+                element.classList.remove('active');
+            } else {
+                content.style.display = 'block';
+                element.classList.add('active');
+            }
+        }
